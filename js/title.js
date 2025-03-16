ce6f34811a4c95bcd484b47c9ebd8958fd82590a
Chenyangtao <786645049@qq.com>
2025-03-16 16:16:15 +0800

    Site updated: 2025-03-16 16:16:15

--- js/title.js
+++ js/title.js
@@ -6,7 +6,7 @@ document.addEventListener('visibilitychange', function () {
 
  if (document.hidden) {
 
- document.title  =  '(ง •̀_•́)ง‼这里是mRNA的Blog~';
+ document.title  =  '(ง •̀_•́)ง‼这里是杨桃非桃的Blog~';
 
  clearTimeout(titleTime);
 
